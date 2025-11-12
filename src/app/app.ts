cc577faead6260fee4cd0c82d49617172e749629
Krzysztof Platis <platonn.git@gmail.com>
2025-11-12 22:40:40 +0100

    child component created programatically via vcr.createComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewContainerRef } from '@angular/core';
 import { ChildComponent } from './child.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ChildComponent],
+  // imports: [ChildComponent],
   template: `
     <h1>App</h1>
-    <app-child></app-child>
+    <!-- <app-child></app-child> -->
   `,
 })
-export class App {}
+export class App {
+  vcr = inject(ViewContainerRef);
+
+  ngOnInit() {
+    this.vcr.createComponent(ChildComponent);
+  }
+}

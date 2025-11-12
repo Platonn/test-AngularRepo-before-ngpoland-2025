21f9427682e75672543a03387d93a61d63b9df13
Krzysztof Platis <platonn.git@gmail.com>
2025-11-13 00:45:17 +0100

    lazy load child component, with waiting via PendingTasks for this component to be created. Thanks to this, hydration is triggered only afterwards, taking this manually-lazy-loaded component into consideration and hydrating it (including the application of the partial hydration)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,27 @@
-import { Component, inject, ViewContainerRef } from '@angular/core';
+import { Component, inject, PendingTasks, ViewChild, ViewContainerRef } from '@angular/core';
+// import { ChildComponent } from './child.component';
 
 @Component({
   selector: 'app-root',
   // imports: [ChildComponent],
   template: `
     <h1>App</h1>
-    <!-- <app-child></app-child> -->
+    <ng-template #childContainer></ng-template>
   `,
 })
 export class App {
-  vcr = inject(ViewContainerRef);
+  @ViewChild('childContainer', { read: ViewContainerRef, static: true })
+  childVcr: ViewContainerRef | undefined;
+
+  pendingTasks = inject(PendingTasks);
 
   ngOnInit() {
-    import('./child.component')
-      .then((m) => m.ChildComponent)
-      .then((lazyLoadedComponent) => {
-        this.vcr.createComponent(lazyLoadedComponent);
-      });
+    this.pendingTasks.run(() =>
+      import('./child.component')
+        .then((m) => m.ChildComponent)
+        .then((lazyLoadedComponent) => {
+          this.childVcr?.createComponent(lazyLoadedComponent);
+        })
+    );
   }
 }
